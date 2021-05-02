87a8bfdcd34f0f3c07d6d7b5f987a3ac2d9e57b3
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-05-01 23:00:06 -0600

    Creacion de Usuarios
    Crea usuario en firebase. Solo 1 se puede generar al parecer.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@ import AppAdmin from './AppAdmin';
 import AppUser from './AppUser';
 import Login from './components/Usuarios/Login/Login';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCiIBhxCtGK1HyEbl8tlZmww6R2XOzs_-w",
+    authDomain: "usuariosweb-64078.firebaseapp.com",
+    projectId: "usuariosweb-64078",
+    storageBucket: "usuariosweb-64078.appspot.com",
+    messagingSenderId: "174839661815",
+    appId: "1:174839661815:web:25d0b439e46f7ea50d7ff2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component {
+
+
+    SignIn = (email, pass) =>{
+        const auth = firebase.auth();
+
+        const promise = auth.createUserWithEmailAndPassword(email, pass)
+        promise.catch(e => console.log(e.message));
+    }
+
+    RegisterIn = (email, pass) =>{
+        const auth = firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(email, pass)
+        promise.catch(e => console.log(e.message));
+    }
+
     render() {
         return (
             <div>
-                <Login />
+                <Login credenciales = {this.SignIn} register = {this.RegisterIn}/>
             </div>
         )
     }
--- src/components/Usuarios/Login/Login.js
+++ src/components/Usuarios/Login/Login.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './style.css';
 
+import { FaSignInAlt } from "react-icons/fa";
+
 class Login extends React.Component {
     constructor(props) {
       super(props);
       this.props = props;
     }
   
+    Credencial = (x, y) =>{
+      this.props.credenciales(x, y);
+    }
+
+    Registro = (x, y) =>{
+      this.props.register(x, y);
+    }
+
     render() {
       return (
         <div className="Main_Login">
@@ -15,9 +25,9 @@ class Login extends React.Component {
                 <label htmlFor="inputEmail" className="sr-only">Email address</label>
                 <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
                 <label htmlFor="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-                <button className="btn btn-lg btn-primary btn-block btn-user" type="submit">Sign in as Admin</button>
-                <button className="btn btn-lg btn-primary btn-block btn-admin" type="submit">
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" defaultValue = "" required />
+                <button className="btn btn-lg btn-primary btn-block btn-user" type="submit" onClick = {() => this.Registro(document.getElementById("inputEmail").value, document.getElementById("inputPassword").value)}>Sign in as Admin</button>
+                <button className="btn btn-lg btn-primary btn-block btn-admin" type="submit" onClick = {() => this.Credencial(document.getElementById("inputEmail").value, document.getElementById("inputPassword").value)}>
                   <div className="google-icon-wrapper">
                     <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="gugul"/>
                   </div>
