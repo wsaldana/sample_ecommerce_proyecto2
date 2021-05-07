bf7ab5a6f0d1880bd2eb3d127cc19f0b70fd6446
juan pablo <jupapineda@gmail.com>
2021-05-07 17:51:22 -0600

    Archivos de Ecommerce

---
+++ src/components/Ecommerce/EcommerceData.js
@@ -0,0 +1,11 @@
+//import React from 'react';
+import wine from '../../assets/images/wine.jpg';
+
+export const EcommerceData = [
+    {
+        title: 'Wine',
+        descr: 'This is Wine',
+        img: wine,
+        price: '$59.99',
+    }
+]
