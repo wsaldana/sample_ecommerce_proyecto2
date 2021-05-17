291765a9eb95c54d3d1572d693d04166fbb431ee
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2021-05-17 12:52:35 -0600

    Empezar backend en Panel Chat

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+//import { Container, Row, Col } from 'react-bootstrap';
 import './grid.css';
 import User from './images/user.png';
-function prueba() {
+import { db, firebase, auth } from "./../../config/firebase.config";
+
+function Prueba() {
+    const [chatState, setChatState] = useState("Finished");
+    const getChats = async () => {
+        db.collection('chats').doc("3U1KybtBjT3DSRcx6xjl")
+          .onSnapshot((snapshot) => {
+            const { status, clientEmail, adminEmail } = snapshot.data();
+            setChatState(status);
+    
+            
+          })
+          console.log(chatState)
+    }
+    const getChat = async () => {
+        db.collection('chats').get().then(querySnapshot=>{
+            //const documents = querySnapshot.docs.map(doc=>doc.data())
+            const documents = querySnapshot.docs.map((doc)=>{
+                console.log(doc.id)
+                console.log(doc.data().status)
+                console.log(doc.data().adminEmail)
+                console.log(doc.data().clientEmail)
+               })
+            //console.log(documents[0].users[0])
+        })
+       
+          
+    }
+    useEffect(() => {
+        getChat();
+        //getChats();
+      });
     const elements = [Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios(), Usuarios()];
     return (
         <div className="divCentral">
@@ -20,7 +51,7 @@ function Usuarios() {
 
 
     return (
-
+        /*
         <Container fluid className="grid">
             <Row justify="between">
                 <Col className="Usuarios">
@@ -55,10 +86,11 @@ function Usuarios() {
 
         </Container>
 
-
+        */
+       <div>Hola Mundo</div>
     )
 
 }
 
 
-export default prueba;
+export default Prueba;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import AppAdmin from './AppAdmin';
 import AppUser from './AppUser';
 import Login from './components/Usuarios/Login/Login';
 import Ecommerce from './components/Ecommerce/Ecommerce';
-
+import Panel from './components/Panel/Panel';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -15,6 +15,7 @@ ReactDOM.render(
           <Route path="/user" component={AppUser} />
           <Route path="/admin" component={AppAdmin} />
           <Route path='/shop' component={Ecommerce} />
+          <Route path='/panel' component={Panel} />
         </Switch>
     </BrowserRouter>
   </React.StrictMode>,
