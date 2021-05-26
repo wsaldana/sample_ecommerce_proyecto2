b7ad1f0f2713c54a4b507c85051c5b0ee26a21ac
Zaray <cor191025@uvg.edu.gt>
2021-05-26 13:22:42 -0600

    estilizando el chat

--- src/components/Chat/Message/Message.jsx
+++ src/components/Chat/Message/Message.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./Message.css";
 
 export default function Message(props) {
-  const { content, isSender } = props;
+  const { content, isSender, time } = props;
   let typeOfMessage = "";
   if (isSender){
-    typeOfMessage = "message-sender bg-primary";
+    typeOfMessage = "message-sender";
   } else {
-    typeOfMessage = "message-receiver bg-secondary";
+    typeOfMessage = "message-receiver";
   }
 
   return (
-    <div className={typeOfMessage}>
-      {content}
+    <div className={`d-flex align-items-end ${typeOfMessage}`}>
+      <p className="m-0">{content}</p>
+      <p className="time ms-3">{time}</p>
     </div>
   )
 }
